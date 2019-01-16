c2aae62580f8b600dd8c0069c5a93f8ba8de5230
Beth <ersergeant@gmail.com>
2019-01-16 12:02:25 -0500

    starter code

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Chicken Burger', false);
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Salmon Burger', false);
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon, Mushroom Burger', false);
+
+
+
