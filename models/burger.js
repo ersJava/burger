80bf36f0a38f684ac17e6ec1cd3a2e09ff1d6671
Beth <ersergeant@gmail.com>
2019-01-24 16:44:14 -0500

    fixing starter code

---
+++ models/burger.js
@@ -0,0 +1,18 @@
+var orm = require("..config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll('burgers', function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+}
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var express = require("express");
-var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
 var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-var connection = mysql.createConnection({
-host: "localhost",
-port: 3306,
-user: "root",
-password: "think&gr0W$",
-database: "burgers_db"
-});
+app.use(routes);
 
-
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-console.log("Server listening on: http://localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
